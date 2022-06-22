e0a4e6f75e62750d201a51e07a63e8022da582f2
David Just <57944742+djust270@users.noreply.github.com>
2022-06-22 16:30:37 -0400

    Create Debian-InstallCaddy.sh

---
+++ Debian-InstallCaddy.sh
@@ -0,0 +1,11 @@
+apt update
+apt install curl
+apt install gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
+apt update
+apt install caddy
+# enable caddy service
+systemctl enable caddy
+# reload caddy service
+systemctl reload caddy
